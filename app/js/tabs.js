db70b568a121baebe04ecef89b7ff5008d86f581
Mur <murmavilink@gmail.com>
2023-05-04 15:12:30 +0600

    done tabs.js

--- app/js/tabs.js
+++ app/js/tabs.js
@@ -1,9 +1,14 @@
 const tabs = () => {
-    const tabBtns = document.querySelectorAll('.tabs__top-btn');
-    const tabContents = document.querySelectorAll('.tabs__content-item');
-    const dateItems = document.querySelectorAll('.cards__day');
-    const contentItems = document.querySelectorAll('.cards__place-wrap');
-    const arrowBtn = document.querySelector('.cards__circle-arrow');
+
+    let parentBlock;
+
+    let tabBtns = document.querySelectorAll('.tabs__top-btn');
+    let tabContents = document.querySelectorAll('.tabs__content-item');
+    
+    let dateItems = document.querySelectorAll('.cards__day');
+    let contentItems = document.querySelectorAll('.cards__place-wrap');
+    let arrowBtns = document.querySelectorAll('.cards__circle-arrow');
+
 
 
     const removeClassActive = (items, btnActiveClass, contents, contentActiveClass) => {
@@ -12,15 +17,25 @@ const tabs = () => {
 
             contents[i].classList.remove(contentActiveClass);
         }
-    }
+    };
 
+    
     const addClassActive = (item, proporty, btnClassActive, contentActiveClass) => {
         const id = item.dataset[proporty];
-        const content = document.getElementById(id);
+        const content = parentBlock.querySelector('#' + id);
 
         item.classList.add(btnClassActive);
         content.classList.add(contentActiveClass);
-    }
+    };
+
+
+    const overwritingElements = (e, parentClass) => {
+        parentBlock = e.target.closest(parentClass);
+
+        dateItems = parentBlock.querySelectorAll('.cards__day');
+        contentItems = parentBlock.querySelectorAll('.cards__place-wrap');
+    };
+
 
     const handlerClickArrow = (idNum) => {
         if (idNum < dateItems.length) {
@@ -30,34 +45,41 @@ const tabs = () => {
             dateItems[0].classList.add('cards__day--active');
             contentItems[0].classList.add('cards__place-wrap--active');
         }
-    }
-
+    };
 
 
     tabBtns.forEach(btn => {
         btn.addEventListener('click', (e) => {
+            overwritingElements(e, '.tabs');
+
             removeClassActive(tabBtns, 'tabs__top-btn--active', tabContents, 'tabs__content-item--active');
             addClassActive(btn, 'item', 'tabs__top-btn--active', 'tabs__content-item--active');
         });
-    })
+    });
 
 
     dateItems.forEach(item => {
-        item.addEventListener('click', () => {
+        item.addEventListener('click', (e) => {
+            overwritingElements(e, '.cards__right');
+
             removeClassActive(dateItems, 'cards__day--active', contentItems, 'cards__place-wrap--active');
             addClassActive(item, 'day', 'cards__day--active', 'cards__place-wrap--active');
-        })
+        });
     });
 
 
-    arrowBtn.addEventListener('click', () => {
-        const idNum = document.querySelector('.cards__day--active').dataset.day.substring(14);
+    arrowBtns.forEach(btn => {
+        btn.addEventListener('click', (e) => {
+            overwritingElements(e, '.cards__right');
+            const idNum = parentBlock.querySelector('.cards__day--active').dataset.day.substring(14);
 
-        removeClassActive(dateItems, 'cards__day--active', contentItems, 'cards__place-wrap--active');
-        handlerClickArrow(idNum);
+            removeClassActive(dateItems, 'cards__day--active', contentItems, 'cards__place-wrap--active');
+            handlerClickArrow(idNum);
+        });
     });
 
-}
+
+};
 
 
 tabs();
